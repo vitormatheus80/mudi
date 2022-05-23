2b868e6013a66b933e6f0e632181a767dfb764ab
Vitor Almeida <vitormatheus80@gmail.com>
2022-05-23 18:18:51 -0300

    update aula 6

--- src/main/java/com/curso/mudi/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.curso.mudi.repository;
-
-import com.curso.mudi.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository <Pedido, Long>{
-}
